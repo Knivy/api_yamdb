c2c53e343434d73cc88409e2be90aec4b22dd696
Albina Giliazova <giliazova@mail.ru>
2024-07-08 18:07:13 +0500

    Small fixes

--- reviews/apps.py
+++ reviews/apps.py
@@ -1,6 +1,7 @@
-from django.apps import AppConfig
+from django.apps import AppConfig  # type: ignore
 
 
 class ReviewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reviews'
+    verbose_name = 'Ревью'
