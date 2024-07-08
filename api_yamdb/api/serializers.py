f47c7f687bcdb0ae051ce2b91b5a6e22aa2adb2e
Albina Giliazova <giliazova@mail.ru>
2024-07-08 20:57:13 +0500

    Title

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -5,7 +5,7 @@ from rest_framework.relations import SlugRelatedField  # type: ignore
 from django.contrib.auth import get_user_model  # type: ignore
 from rest_framework.validators import UniqueTogetherValidator  # type: ignore
 
-from reviews.models import Category, Genre
+from reviews.models import Category, Genre, Title
 
 User = get_user_model()
 
@@ -26,3 +26,36 @@ class GenreSerializer(serializers.ModelSerializer):
         model = Genre
         fields = ('name', 'slug')
         lookup_field = 'slug'
+
+
+class TitleWriteSerializer(serializers.ModelSerializer):
+    """Сериализатор произведений на запись."""
+
+    category = SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all(),
+    )
+    genre = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Genre.objects.all(),
+        many=True,
+    )
+
+    class Meta:
+        model = Title
+        fields = ('name', 'year', 'description', 'category', 'genre')
+
+
+class TitleReadSerializer(serializers.ModelSerializer):
+    """Сериализатор произведений на чтение."""
+
+    # Надо еще добавить rating.
+
+    category = CategorySerializer()
+    genre = GenreSerializer(
+        many=True,
+    )
+
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'description', 'category', 'genre')
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,13 +3,15 @@
 from rest_framework import viewsets, generics, filters  # type: ignore
 from rest_framework.pagination import LimitOffsetPagination  # type: ignore
 from django.shortcuts import get_object_or_404  # type: ignore
-from rest_framework.permissions import IsAuthenticated  # type: ignore
 from django.contrib.auth import get_user_model  # type: ignore
 from django.http import JsonResponse  # type: ignore
+from django_filters.rest_framework import DjangoFilterBackend  # type: ignore
 
-from reviews.models import Category, Genre
-from .serializers import (CategorySerializer, GenreSerializer)
-from .permissions import AdminOrReadOnlyPermission
+from reviews.models import Category, Genre, Title
+from .serializers import (CategorySerializer, GenreSerializer,
+                          TitleReadSerializer, TitleWriteSerializer)
+from .permissions import (AdminOrReadListOnlyPermission,
+                          AdminOrReadOnlyPermission)
 
 User = get_user_model()
 
@@ -19,7 +21,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = (AdminOrReadOnlyPermission,)
+    permission_classes = (AdminOrReadListOnlyPermission,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'  # Чтобы адрес был вида /categories/{slug}/
@@ -30,7 +32,22 @@ class GenreViewSet(viewsets.ModelViewSet):
 
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = (AdminOrReadOnlyPermission,)
+    permission_classes = (AdminOrReadListOnlyPermission,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """Обработка произведений."""
+
+    queryset = Title.objects.all()
+    permission_classes = (AdminOrReadOnlyPermission,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('name', 'year',
+                        'genre__slug', 'category__slug')
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return TitleReadSerializer
+        return TitleWriteSerializer
